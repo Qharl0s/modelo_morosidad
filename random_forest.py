610e2ae92bfcb08501ffcc953002d13c8f475d9e
alarico <alarico@cajalosandes.pe>
2025-10-23 21:48:44 -0500

    graficos

--- random_forest.py
+++ random_forest.py
@@ -58,3 +58,52 @@ sns.barplot(x=feat_importances.values, y=feat_importances.index)
 plt.title("Importancia de las variables")
 plt.tight_layout()
 plt.show()
+
+# Visualizar uno de los árboles del bosque
+plt.figure(figsize=(20, 10))  # Ajusta el tamaño según necesidad
+plot_tree(
+    model.estimators_[0],                # Uno de los árboles del Random Forest
+    feature_names=X.columns,             # Nombres de las variables
+    class_names=[str(cls) for cls in model.classes_],  # Nombres de clases
+    filled=True,                         # Colores según clase
+    rounded=True,                        # Bordes redondeados
+    fontsize=6                          # Tamaño de letra
+)
+plt.title("Árbol de decisión del Random Forest (estimador 0)")
+plt.show()
+
+# Histograma de Cuotas Atradas vs Moroso
+plt.figure(figsize=(10, 5))
+sns.histplot(data=df, x='nEdad', hue='Moroso', multiple="stack", bins=20)
+plt.title("Cuotas atrasadas según nivel de morosidad")
+plt.xlabel("Cuotas atrasadas")
+plt.ylabel("Frecuencia")
+plt.show()
+
+# Matriz de Dispersion
+# Filtrar solo morosos de nivel 3 y 4
+df_filtrado = df[df['Moroso'].isin([3, 4])]
+
+
+if 'cProducto' in label_encoders:
+    df_filtrado['cProducto'] = label_encoders['cProducto'].inverse_transform(df_filtrado['cProducto'])
+
+# Gráfico de dispersión
+plt.figure(figsize=(12, 6))
+sns.stripplot(
+    data=df_filtrado,
+    x='cProducto',           # Variable categórica en eje X
+    y='nCapitalDesembolso',        # Variable numérica en eje Y
+    hue='Moroso',          # Color según nivel de morosidad
+    jitter=True,            # Desplaza puntos horizontalmente para que no se encimen
+    dodge=True,
+    palette='coolwarm',
+    alpha=0.7
+)
+plt.title("Producto x Desembolso (Morosos Nivel 3 y 4)")
+plt.xlabel("Producto")
+plt.ylabel("Desembolso")
+plt.legend(title="Moroso")
+plt.grid(True, axis='y')
+plt.tight_layout()
+plt.show()
